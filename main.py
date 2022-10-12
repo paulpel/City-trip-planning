86a418dc0114354b3e5d69f9214981638e5dbd28
Pawel Pelar <pawelpelar@gmail.com>
2022-10-12 18:11:18 +0200

    Load data. App basic config

--- main.py
+++ main.py
@@ -1,6 +1,50 @@
 import json
+import logging
+import sys
+import pandas as pd
+from typing import Dict
 
-with open("attractions.json") as jf:
-    data = json.load(jf)
 
-print(len(data['rome']))
+class CityTrip:
+
+    def __init__(self) -> None:
+        """Init function
+        """
+        logging.basicConfig(
+            format="[%(asctime)s] [%(levelname)s] [%(filename)s %(funcName)s:%(lineno)d] Msg: %(message)s")
+
+        self.cities = set()
+        self.attractions = self.load_data()
+
+    def main(self) -> None:
+        """Main function of City Planning App
+        """
+        pass
+
+    def load_data(self) -> Dict:
+        """Load data from json
+
+        :return: attractions mapped to cities
+        :rtype: Dict
+        """
+        with open("attractions.json") as jf:
+            data = json.load(jf)
+
+        cities = {}
+        for city in data:
+            self.cities.add(city)
+
+            cities[city] = pd.DataFrame.from_dict(
+                data[city], orient='columns', dtype=None, columns=None)
+
+        if cities:
+            return cities
+        else:
+            logging.error("Data empty!")
+            sys.exit(-1)
+
+
+if __name__ == "__main__":
+
+    ct_obj = CityTrip()
+    ct_obj.main()
