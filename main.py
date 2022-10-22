931fe61c13f816a09d507ca07763cb72bfb3d416
Pawel Pelar <pawelpelar@gmail.com>
2022-10-22 18:40:53 +0200

    Json refactor. Distances calculated

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import copy
 import json
 import logging
 import sys
-import pandas as pd
 from typing import Dict
+
+import geopy.distance
 
 
 class CityTrip:
@@ -13,13 +15,16 @@ class CityTrip:
         logging.basicConfig(
             format="[%(asctime)s] [%(levelname)s] [%(filename)s %(funcName)s:%(lineno)d] Msg: %(message)s")
 
-        self.cities = set()
-        self.attractions = self.load_data()
+        self.distances = {}
+        self.choosen_city = "rome"
+
+        self.data = self.load_data()
+        self.cities = list(self.data.keys())
 
     def main(self) -> None:
         """Main function of City Planning App
         """
-        pass
+        self.calc_distances()
 
     def load_data(self) -> Dict:
         """Load data from json
@@ -30,18 +35,35 @@ class CityTrip:
         with open("attractions.json") as jf:
             data = json.load(jf)
 
-        cities = {}
-        for city in data:
-            self.cities.add(city)
-
-            cities[city] = pd.DataFrame.from_dict(
-                data[city], orient='columns', dtype=None, columns=None)
-
-        if cities:
-            return cities
+        if data:
+            return data
         else:
             logging.error("Data empty!")
             sys.exit(-1)
+
+    def calc_distances(self):
+        """Calculate distances beetween all attraction combination
+        """
+        all_attractions = list(self.data[self.choosen_city].keys())
+
+        copy_all_attractions = copy.deepcopy(all_attractions)
+
+        while copy_all_attractions:
+            attraction_a = copy_all_attractions.pop()
+            self.distances[attraction_a] = {}
+            cords_a = (
+                self.data[self.choosen_city][attraction_a]["cords"]["latitude"],
+                self.data[self.choosen_city][attraction_a]["cords"]["longitude"]
+                )
+
+            for attraction_b in all_attractions:
+                if attraction_a != attraction_b:
+                    cords_b = (
+                        self.data[self.choosen_city][attraction_b]["cords"]["latitude"],
+                        self.data[self.choosen_city][attraction_b]["cords"]["longitude"]
+                        )
+                    self.distances[attraction_a][attraction_b] = \
+                        geopy.distance.geodesic(cords_a, cords_b).km
 
 
 if __name__ == "__main__":
